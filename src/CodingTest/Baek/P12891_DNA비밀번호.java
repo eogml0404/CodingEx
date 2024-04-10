b4189e63916c8a0c41ebd4d54c335da4706711f8
eogml0404 <eogml0404@naver.com>
2024-04-10 23:55:31 +0900

    to Array

---
+++ src/CodingTest/Baek/P12891_DNA비밀번호.java
@@ -0,0 +1,24 @@
+package CodingTest.Baek;
+
+import java.util.Scanner;
+
+public class P12891_DNA비밀번호 {
+
+    static int checkArr[];
+    static int myArr[];
+    static int checkSecret;
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int S = sc.nextInt();
+        int P = sc.nextInt();
+        int Result = 0;
+        char A[] = new char[5];
+        checkArr = new int[4];
+        myArr = new int[4];
+
+        
+        sc.close();
+    }
+}
